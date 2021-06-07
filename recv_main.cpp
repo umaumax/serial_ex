7f6aa9184e7800ef75ad636275b9fb83c49bd13e
umaumax <umauma7083@gmail.com>
2021-06-07 23:32:35 +0900

    Add all baudrate function to recv_main

--- recv_main.cpp
+++ recv_main.cpp
@@ -8,20 +8,19 @@
 #include <cstdlib>
 #include <cstring>
 #include <iostream>
+#include <map>
 #include <thread>
+#include <vector>
 
-#define BAUDRATE B9600
-#define SERIAL_PORT "/dev/ttyUSB0"
-
-int main(int argc, char *argv[]) {
+bool recv_serial(std::string serial_port, speed_t baudrate, int read_num = -1) {
   int fd;
   struct termios tio_bk, tio;
 
   // open read/write for echo back
-  fd = open(SERIAL_PORT, O_RDWR);
+  fd = open(serial_port.c_str(), O_RDWR);
   if (fd < 0) {
     printf("open error\n");
-    return -1;
+    return false;
   }
 
   int ret;
@@ -30,7 +29,7 @@ int main(int argc, char *argv[]) {
   ret = tcgetattr(fd, &tio_bk);
   if (ret == -1) {
     std::cerr << "failed tcgetattr:" << std::strerror(errno) << std::endl;
-    return 1;
+    return false;
   }
 
   memset(&tio, 0, sizeof(tio));
@@ -38,35 +37,37 @@ int main(int argc, char *argv[]) {
   tio.c_iflag    = IGNPAR;
   tio.c_cc[VMIN] = 1;
 
-  ret = cfsetspeed(&tio, BAUDRATE);
+  ret = cfsetspeed(&tio, baudrate);
   if (ret == -1) {
     std::cerr << "failed cfsetspeed:" << std::strerror(errno) << std::endl;
-    return 1;
+    return false;
   }
 
   ret = tcsetattr(fd, TCSANOW, &tio);
   if (ret == -1) {
     std::cerr << "failed tcsetattr:" << std::strerror(errno) << std::endl;
-    return 1;
+    return false;
   }
 
   ret = tcflush(fd, TCIFLUSH);
   if (ret == -1) {
     std::cerr << "failed tcflush:" << std::strerror(errno) << std::endl;
-    return 1;
+    return false;
   }
 
   int len;
   char buf[256];
-  for (int i = 0; true; i++) {
+  for (int i = 0; read_num == -1 || i < read_num; i++) {
     len = read(fd, buf, sizeof(buf));
     if (len == 0) {
-      // waiting...
-      std::cout << ".";
+      if (read_num == -1) {
+        // waiting...
+        std::cout << ".";
+      }
       continue;
     } else if (len < 0) {
       std::cerr << "failed read:" << std::strerror(errno) << std::endl;
-      return 1;
+      return false;
     } else {
       std::cout << "[" << i << "] " << std::endl;
       printf("[len=%d]", len);
@@ -81,7 +82,7 @@ int main(int argc, char *argv[]) {
     int ret = write(fd, buf, len);
     if (ret == -1) {
       std::cerr << "failed write:" << std::strerror(errno) << std::endl;
-      return 1;
+      return false;
     }
   }
 
@@ -89,12 +90,59 @@ int main(int argc, char *argv[]) {
   ret = tcsetattr(fd, TCSANOW, &tio_bk);
   if (ret == -1) {
     std::cerr << "failed tcsetattr:" << std::strerror(errno) << std::endl;
-    return 1;
+    return false;
   }
   ret = close(fd);
   if (ret == -1) {
     std::cerr << "failed close:" << std::strerror(errno) << std::endl;
+    return false;
+  }
+  return true;
+}
+
+int main(int argc, char* argv[]) {
+  if (argc < 3) {
+    std::cerr << argv[0] << " serial_port baudrate" << std::endl;
+    std::cerr << "e.g. serial_port='/dev/ttyUSB0'" << std::endl;
+    std::cerr << "     baudrate=9600" << std::endl;
+    std::cerr << "     baudrate=-1 means try all baudrate" << std::endl;
     return 1;
+  }
+  std::string serial_port = std::string(argv[1]);
+  int baudrate_number     = std::stoi(std::string(argv[2]));
+
+  // NOTE: full list: 110, 150, 300, 1200, 2400, 4800, 9600, 19200, 38400, 57600, 115200, 230400, 460800, 921600
+  std::map<int, speed_t> baudrate_map = {
+      {4800, B4800},   {9600, B9600},   {9600, B9600},     {19200, B19200},
+      {38400, B38400}, {57600, B57600}, {115200, B115200}, {230400, B230400},
+  };
+  if (baudrate_number != -1) {
+    auto it = baudrate_map.find(baudrate_number);
+    if (it == baudrate_map.end()) {
+      std::cerr << "valid baudrate values: [";
+      for (auto& v : baudrate_map) {
+        std::cerr << v.first << ", ";
+      }
+      std::cerr << "]" << std::endl;
+      return 1;
+    }
+    speed_t baudrate = it->second;
+    bool ret         = recv_serial(serial_port, baudrate);
+    if (!ret) {
+      return 1;
+    }
+  } else {
+    // try all baudrate
+    int read_num = 8;
+    for (auto& v : baudrate_map) {
+      int baudrate_number = v.first;
+      speed_t baudrate    = v.second;
+      std::cout << "baudrate:" << baudrate_number << std::endl;
+      bool ret = recv_serial(serial_port, baudrate, read_num);
+      if (!ret) {
+        return 1;
+      }
+    }
   }
   return 0;
 }
